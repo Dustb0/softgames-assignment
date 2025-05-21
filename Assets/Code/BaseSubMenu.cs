64109b5e8ce51ed62a0f35211cb36c8d6697695d
Manuel <manuel.schmuki@gmail.com>
2025-05-21 17:20:16 +0200

    Add FPS Counter

--- Assets/Code/BaseSubMenu.cs
+++ Assets/Code/BaseSubMenu.cs
@@ -6,18 +6,39 @@ public abstract class BaseSubMenu : MonoBehaviour
 {
     protected UIDocument uiDocument;
     private Button backToTitleButton;
+    private Label fpsLabel;
+
+    int frameCount;
+    float fpsElapsedTime;
+    float fpsUpdateInterval = 1;
 
     protected virtual void Start()
     {
         uiDocument = GetComponent<UIDocument>();
         backToTitleButton = uiDocument.rootVisualElement.Q<Button>("BackToTitleButton");
         backToTitleButton.RegisterCallback<ClickEvent>(OnClickTitleButton);
+
+        fpsLabel = uiDocument.rootVisualElement.Q<Label>("FPSLabel");
+        fpsLabel.text = "";
+    }
+
+    protected virtual void Update() 
+    {
+        // Only update FPS display in certain intervals for better readability 
+        frameCount++;
+        fpsElapsedTime += Time.unscaledDeltaTime;
+        if (fpsElapsedTime >= fpsUpdateInterval)
+        {
+            var fps = Mathf.FloorToInt(frameCount / fpsElapsedTime);
+            fpsLabel.text = $"FPS: {fps}";
+            frameCount = 0;
+            fpsElapsedTime = 0;
+        }     
     }
 
     private void OnClickTitleButton(ClickEvent evt)
     {
         TitleMenu.BackToTitle();
     }
-
    
 }
